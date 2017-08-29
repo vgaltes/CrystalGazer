291f7dc8e76b17195dcc234e2d43ffa8ff9f1b7c
Vicenç García-Altés <vgaltes@thoughtworks.com>
2017-08-29 16:30:17 +0100

    Add line chart visualization for complexity over time.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 const program = require('commander');
 const cg = require('./src/crystalgazer');
 const pkg = require('./package.json');
+const dateFns = require('date-fns');
 const blessed = require('blessed');
 const contrib = require('blessed-contrib');
 const screen = blessed.screen();
-const grid = new contrib.grid({rows: 1, cols: 1, screen: screen});
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
     return process.exit(0);
@@ -25,6 +25,32 @@ let getConfigFrom = function(configName, options){
     };
 
     return cgConfig;
+};
+
+let drawTwoColumnTable = function(headers, data){
+    const grid = new contrib.grid({rows: 1, cols: 1, screen: screen});
+    const table = grid.set(0, 0, 1, 1,contrib.table,
+        { keys: true
+        , fg: 'white'
+        , selectedFg: 'white'
+        , selectedBg: 'blue'
+        , interactive: true
+        , label: 'Revisions by file (Press ESC to exit)'
+        , width: '50%'
+        , height: '50%'
+        , border: {type: "line", fg: "cyan"}
+        , columnSpacing: 5 //in chars
+        , columnWidth: [60, 20] /*in chars*/ });
+
+   
+    //allow control the table with the keyboard
+    table.focus();
+   
+    table.setData(
+      { headers: headers
+      , data: data});     
+
+    screen.render();
 };
 
 let init = function(configName, options){
@@ -61,31 +87,6 @@ let authors = function(configName, options){
     console.log('authors: ' + authors);
 };
 
-let drawTwoColumnTable = function(headers, data){
-    var table = grid.set(0, 0, 1, 1,contrib.table,
-        { keys: true
-        , fg: 'white'
-        , selectedFg: 'white'
-        , selectedBg: 'blue'
-        , interactive: true
-        , label: 'Revisions by file (Press ESC to exit)'
-        , width: '50%'
-        , height: '50%'
-        , border: {type: "line", fg: "cyan"}
-        , columnSpacing: 5 //in chars
-        , columnWidth: [60, 20] /*in chars*/ });
-
-   
-      //allow control the table with the keyboard
-      table.focus()
-   
-      table.setData(
-      { headers: headers
-      , data: data});     
-
-  screen.render()
-};
-
 let revisionsByFile = function(configName, options){
     const cgConfig = getConfigFrom(configName, options);    
     const revisionsByFile = cg.revisionsByFile(cgConfig);
@@ -113,13 +114,55 @@ let authorsByFile = function(configName, options){
     const cgConfig = getConfigFrom(configName, options);    
     const authorsByFile = cg.authorsByFile(cgConfig);
 
-    console.log('authorsByFile: ' + JSON.stringify(authorsByFile));
+    const headers = ['File name', '# Authors'];
+    const data = authorsByFile.map(function(item){
+        return [item.file, item.authors];
+    });
+
+    drawTwoColumnTable(headers, data);
 };
+
+let addLineChartTo = function(grid, xpos, ypos, label, series){
+    let line = grid.set(xpos, ypos, 1, 1,contrib.line,
+        { style:
+          { line: "yellow"
+          , text: "green"
+          , baseline: "black"}
+        , xLabelPadding: 3
+        , xPadding: 5
+        , showLegend: false
+        , wholeNumbersOnly: true //true=do not show fraction in y axis
+        , label: label});
+
+        screen.append(line);
+        line.setData([series]);
+}
 
 let complexityOverTime = function(configName, fileName, options){
     const cgConfig = getConfigFrom(configName, options);    
     cg.complexityOverTime(cgConfig, fileName).then(function(complexity){
-        console.log('complexity trend: ' + JSON.stringify(complexity));
+        const grid = new contrib.grid({rows: 2, cols: 1, screen: screen});
+        
+        var linesSeries = {
+            title: 'Lines',
+            x: complexity.map(function(c){return c.date;}),
+            y: complexity.map(function(c){return c.numberOfLines;}),
+            style: {
+                line: 'blue'
+               }
+         };
+        addLineChartTo(grid, 0, 0, 'Complexity (number of lines) over time (Press ESC to exit)',linesSeries);
+
+        var tabsSeries = {
+            title: 'Tabs',
+            x: complexity.map(function(c){return c.date;}),
+            y: complexity.map(function(c){return c.maxNumberOfTabs;}),
+            style: {
+                line: 'red'
+            }
+        };
+        addLineChartTo(grid, 1, 0, 'Complexity (max # of tabs) over time (Press ESC to exit)',tabsSeries);
+        screen.render();
     });    
 };
 
